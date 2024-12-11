f188c23bd7ec0811373186ce496226d90c1a07d7
Lennart Nachtigall <mail@firesurfer.de>
2024-12-11 17:02:38 +0100

    start to rework the singleton so it can be safely used

--- include/ethercat_sdk_master/EthercatMasterSingleton.hpp
+++ include/ethercat_sdk_master/EthercatMasterSingleton.hpp
@@ -12,7 +12,13 @@ namespace ecat_master {
         public:
             static EthercatMasterSingleton&  instance(); //Method has to be in cpp file in order to work properly
 
-            std::shared_ptr<EthercatMaster> get(const EthercatMasterConfiguration& config) {
+            /**
+             * @brief get a shared pointer to an ecat master instance
+             * If there is already a master active for the given instance we simply reuse it and return it
+             * @note Using this methods enforces asynchronous spinning of the master in this class
+             * @note In case the "new" ethercat master configuration does not match the existing one only a warning is printed!
+             */
+            std::shared_ptr<EthercatMaster> get(const EthercatMasterConfiguration& config, int rt_prio = 48) {
                 std::lock_guard<std::recursive_mutex> guard(lock_);
 
                 if(ecat_masters_.find(config.networkInterface) == ecat_masters_.end())
@@ -24,7 +30,7 @@ namespace ecat_master {
                     ecat_masters_[config.networkInterface] = master;
                     
                     //Spin the master asynchronously
-                    spin_threads_.emplace(config.networkInterface, std::make_unique<std::thread>(std::bind(&EthercatMasterSingleton::spin,this, std::placeholders::_1), master));
+                    spin_threads_.emplace(config.networkInterface, std::make_unique<std::thread>(std::bind(&EthercatMasterSingleton::spin,this, std::placeholders::_1, std::placeholders::_2), master, rt_prio));
                 }
                 
                 if(config != ecat_masters_[config.networkInterface]->getConfiguration()){
@@ -32,7 +38,7 @@ namespace ecat_master {
                     MELO_WARN_STREAM("Ethercat master configurations do not match for bus: " << config.networkInterface);
                 }
 
-                return ecat_masters_[config.networkInterface];
+                return ecat_masters_.at(config.networkInterface);
             }
 
             bool hasMaster(const EthercatMasterConfiguration& config){
@@ -44,6 +50,10 @@ namespace ecat_master {
 
             std::shared_ptr<EthercatMaster> operator[] (const EthercatMasterConfiguration& config){
                 return get(config);
+            }
+
+            bool shutdown(std::shared_ptr<EthercatMaster> master){
+                return true;
             }
         private:
 
@@ -61,8 +71,9 @@ namespace ecat_master {
                 master->shutdown();
             }
         }
-        void spin(std::shared_ptr<EthercatMaster> master_){
-            master_->setRealtimePriority(); 
+        void spin(std::shared_ptr<EthercatMaster> master_, int rt_prio){
+            //We override the default rt prio of 99 as this might starve kernel threads. 
+            master_->setRealtimePriority(rt_prio); 
             while(!abort_){
                 master_->update(UpdateMode::StandaloneEnforceRate);
             }
@@ -70,6 +81,7 @@ namespace ecat_master {
 
         std::map<std::string,std::shared_ptr<EthercatMaster>> ecat_masters_;
         std::map<std::string, std::unique_ptr<std::thread>> spin_threads_;
+        std::map<std::string, std::atomic_bool> abort_signals_;
         std::recursive_mutex lock_;
         std::atomic_bool abort_ = false;
     };
